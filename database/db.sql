f6bda388cc88ee34bf7d4ef791545784f6a125a4
altoma123 <tokkto.jr@hotmail.com>
2022-09-06 19:42:49 -0500

    Server & Template Engine config + Mysql Connection + Main templates + basic routes

---
+++ database/db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE db_links;
+
+USE db_links;
+
+-- USERS TABLE
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE users;
+
+-- LINKS TABLE
+CREATE TABLE links (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(150) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    userd_id INT(11),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    PRIMARY KEY(id),
+    CONSTRAINT fk_user FOREIGN KEY(userd_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
