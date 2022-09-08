ac0052561335f6ad7e8e42651c902e8524a93e6a
altoma123 <tokkto.jr@hotmail.com>
2022-09-08 17:44:24 -0500

    Signup for users added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session');
+const passport = require('passport');
 
 const { database } = require('./keys');
 
 
 // Initializations
 const app = express();
+require('./lib/passport');
 
 
 // Settings
@@ -38,6 +40,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // Global Variables
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+//another way to:
+// router.post('/signup', (req, res) => {
+//     passport.authenticate('local.signup', {
+//         successRedirect: '/profile',
+//         failureRedirect: '/signup',
+//         failureFlash: true
+//     });
+// });
+
+router.get('/profile', (req, res) => {
+    res.send('this is your profile');
+});
+
 module.exports = router;
